f5d5c9830b7b792ed078bfc1d490ee346ad63843
Ryudas <Ryudas@users.noreply.github.com>
2020-06-08 06:37:55 +0200

    Update shader_frag.glsl

--- shaders/shader_frag.glsl
+++ shaders/shader_frag.glsl
@@ -1,7 +1,7 @@
 #version 450 core
 
 // Global variables for lighting calculations.
-layout(location = 1) uniform vec3 viewPos;
+//layout(location = 1) uniform vec3 viewPos;
 layout(location = 2) uniform sampler2D tex_drawing;
 
 //layout(location = 3) uniform mat4 lightMVP;
@@ -14,6 +14,7 @@ layout(location = 0) out vec4 outColor;
 in vec3 fragPos; // World-space position
 in vec3 fragNormal; // World-space normal
 layout(origin_upper_left) in vec4 gl_FragCoord; // window relative fragment coordinates
+
 
 
 void make_kernel(inout vec4 n[9], sampler2D tex, vec2 coord)
@@ -32,6 +33,7 @@ void make_kernel(inout vec4 n[9], sampler2D tex, vec2 coord)
 	n[8] = texture(tex, coord + vec2(  w, h));
 }
 
+
 void main()
 {
    // current position
@@ -39,15 +41,22 @@ void main()
 
 
 	vec4 n[9];
+	// construct 3x3 kernel matrix around texture coordinates 
 	make_kernel( n, tex_drawing , tex_coords );
 
-	vec4 sobel_edge_v = n[2] + (2.0*n[5]) + n[8] - (n[0] + (2.0*n[3]) + n[6]);
-  	vec4 sobel_edge_h = n[0] + (2.0*n[1]) + n[2] - (n[6] + (2.0*n[7]) + n[8]);
-	vec4 sobel = sqrt((sobel_edge_h * sobel_edge_h) + (sobel_edge_v * sobel_edge_v));
-	vec3 out_vec =  vec3(length(sobel_edge_v), length(sobel_edge_h),0.0);
-	outColor = vec4( normalize(out_vec), 1.0 );
+	// compute horzontal and vertical gradient components using sobel's operator
+	vec4 sobel_edge_h = n[2] + (2.0*n[5]) + n[8] - (n[0] + (2.0*n[3]) + n[6]);
+  	vec4 sobel_edge_v = n[0] + (2.0*n[1]) + n[2] - (n[6] + (2.0*n[7]) + n[8]);
+	
+	// compute total gradient magnitude
+	//vec4 sobel = sqrt((sobel_edge_h * sobel_edge_h) + (sobel_edge_v * sobel_edge_v));
+	
+	// compute component-wise gradient
+	vec3 out_vec =  normalize (vec3(length(sobel_edge_v), length(sobel_edge_h),0.0) );
+	
+	// render normalized gradient vector as a RG colourspace 
+	outColor = vec4( out_vec, 1.0 );
 
-
-
-  //  outColor =  texture(tex_drawing, tex_coords);
+	// output basic underlying texture 
+	// outColor =  texture(tex_drawing, tex_coords);
 }
